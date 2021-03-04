800fec8f5335e6999f6a07697fdcacd14f2b3f86
damola shomoye <dshomoye@gmail.com>
2021-03-04 11:55:57 -0800

    try to silence with pipefail

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh -l
+set +o pipefail
+
 splunk-appinspect inspect --help
-splunk-appinspect inspect $1 --output-file appinspect_result.json --mode precert || true
+splunk-appinspect inspect $1 --output-file appinspect_result.json --mode precert > /dev/null || true
+echo "checking status of json result"
 python check.py
