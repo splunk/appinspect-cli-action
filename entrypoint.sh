1ffc45643af7b85fe73072190da0fac20722576d
damola shomoye <dshomoye@gmail.com>
2021-03-29 11:08:59 -0700

    move warning block to no failure

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,11 +13,18 @@ def main(args):
                 failures = result[\"summary\"][\"failure\"]
                 if failures == 0:
                     print(\"App Inspect Passed!\")
+                    if \"warning\" in result[\"summary\"] and result[\"summary\"][\"warning\"]:
+                        print(\"Warning List:\")
+                        for group in result[\"reports\"][0][\"groups\"]:
+                            for check in group[\"checks\"]:
+                                if check[\"result\"]==\"warning\":
+                                    for msg in check[\"messages\"]:
+                                        print(msg[\"message\"])
                     print(result[\"summary\"])
-                    print(\"::set-output name=status::success\")
+                    print(\"::set-output name=status::pass\")
                 else:
                     print(f\"App Inspect returned {failures} failures.\")
-                    print(\"::set-output name=time::fail\")
+                    print(\"::set-output name=status::fail\")
                     print(result[\"summary\"])
                     print(\"Failure List:\")
                     for group in result[\"reports\"][0][\"groups\"]:
@@ -28,16 +35,8 @@ def main(args):
                     sys.exit(1)
             else:
                 print(\"Unexpected JSON format\")
-                print(\"::set-output name=time::fail\")
+                print(\"::set-output name=status::fail\")
                 sys.exit(1)
-
-            if \"summary\" in result and \"warning\" in result[\"summary\"]:
-                print(\"Warning List:\")
-                for group in result[\"reports\"][0][\"groups\"]:
-                    for check in group[\"checks\"]:
-                        if check[\"result\"]==\"warning\":
-                            for msg in check[\"messages\"]:
-                                print(msg[\"message\"])
     except Exception as e:
         print(f\"An error occured {str(e)}\")
         sys.exit(1)
