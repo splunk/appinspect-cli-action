f5a3e4421592b6d1a945811f1570467ff8a438d6
uoboda-splunk <uoboda@splunk.com>
2021-10-19 10:31:42 +0200

    adding paramters to check script

--- entrypoint.sh
+++ entrypoint.sh
@@ -47,7 +47,7 @@ echo "test_exit_code $test_exit_code"
 echo "tags: $INPUT_INCLUDED_TAGS"
 if [[ "$INPUT_INCLUDED_TAGS" == *"manual"* ]] && [ $test_exit_code == 0 ]; then
   echo "::group::manual_checks"
-  python3 /compare_checks.py
+  python3 /compare_checks.py $APP_VETTING $INPUT_RESULT_FILE
   test_exit_code=$?
   echo "::endgroup::"
 fi
