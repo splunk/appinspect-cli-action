533469c6fe43819d8626c19480c0c93381e4bad8
rfaircloth-splunk <rfaircloth@splunk.com>
2022-01-20 13:07:12 -0500

    fix: typo

--- compare_checks.py
+++ compare_checks.py
@@ -43,7 +43,7 @@ def compare(
 
     manual_checks = get_checks_from_appinspect_result(appinspect_result_file)
 
-    vetting_date = {}
+    vetting_data = {}
     if os.path.isfile(vetting_file):
         with open(vetting_file) as f:
             vetting_data = yaml.safe_load(f)            
