718b7f5541e6e6c85c3db207b5f94deef04a7017
truptilangalia-crest <79827058+truptilangalia-crest@users.noreply.github.com>
2022-12-16 09:29:35 +0530

    test: updated conditions for checking vetting file

--- entrypoint.sh
+++ entrypoint.sh
@@ -42,25 +42,28 @@ python3 /reporter.py $INPUT_RESULT_FILE
 exit_code=$?
 echo "::endgroup::"
 
-python3 /compare_checks.py $INPUT_APP_VETTING $INPUT_RESULT_FILE "manual_check"
-exit_code=$?
 if [ $exit_code != 0 ]; then
   echo "::group::failure_checks"
   python3 /compare_checks.py $INPUT_APP_VETTING $INPUT_RESULT_FILE "failure"
-  exit_code=$?
+  exit_code_failure=$?
   echo "::endgroup::"
 fi
 
-if [[ "$INPUT_INCLUDED_TAGS" == *"manual"* ]] && [ $exit_code == 0 ]; then
-  echo "::group::manual_checks"
-  python3 /compare_checks.py $INPUT_APP_VETTING $INPUT_RESULT_FILE "manual_check"
-  exit_code=$?
-  if [ $exit_code == 0 ]; then
-    echo "successful comparison, generating markdown"
-    echo "/export_to_markdown.py $INPUT_APP_VETTING $INPUT_MANUAL_CHECK_MARKDOWN"
-    python3 /export_to_markdown.py $INPUT_APP_VETTING $INPUT_MANUAL_CHECK_MARKDOWN
-  fi
-  echo "::endgroup::"
-fi
+echo "::group::manual_checks"
+python3 /compare_checks.py $INPUT_APP_VETTING $INPUT_RESULT_FILE "manual_check"
+exit_code_manual_check=$?
+echo "::endgroup::"
 
-exit "$exit_code"
+# if [[ "$INPUT_INCLUDED_TAGS" == *"manual"* ]] && [ $exit_code == 0 ]; then
+#   echo "::group::manual_checks"
+#   python3 /compare_checks.py $INPUT_APP_VETTING $INPUT_RESULT_FILE "manual_check"
+#   exit_code=$?
+#   if [ $exit_code == 0 ]; then
+#     echo "successful comparison, generating markdown"
+#     echo "/export_to_markdown.py $INPUT_APP_VETTING $INPUT_MANUAL_CHECK_MARKDOWN"
+#     python3 /export_to_markdown.py $INPUT_APP_VETTING $INPUT_MANUAL_CHECK_MARKDOWN
+#   fi
+#   echo "::endgroup::"
+# fi
+
+! (($exit_code_failure || $exit_code_manual_check))
